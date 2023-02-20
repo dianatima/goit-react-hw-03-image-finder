e48bdbd068293466d9c29b6f1d67944d4fff13b7
Yulia <nayanzina@gmail.com>
2023-02-21 01:35:08 +0200

    image-finder

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,20 @@
+import {
+    ImageGalleryItemWrap,
+    ImageGalleryItemImage,
+  } from "./ImageGalleryItem.styled";
+  
+  export const ImageGalleryItem = ({
+    image: { tags, webformatURL, largeImageURL },
+    openModal,
+  }) => {
+    return (
+      <ImageGalleryItemWrap>
+        <ImageGalleryItemImage
+          src={webformatURL}
+          alt={tags}
+          onClick={() => openModal(largeImageURL, tags)}
+        />
+      </ImageGalleryItemWrap>
+    );
+  };
+  
